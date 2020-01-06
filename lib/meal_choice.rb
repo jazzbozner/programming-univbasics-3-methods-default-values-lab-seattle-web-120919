fa76085219c874cff5a59b8958aca521c68a69c1
Jazz Bozner <jazzbozner@gmail.com>
2020-01-06 23:01:28 +0000

    Automatically backed up by Learn

--- lib/meal_choice.rb
+++ lib/meal_choice.rb
@@ -2,5 +2,5 @@
 # For output purposes, use "puts" instead of "print" or "p"
 
 def meal_choice(veg1, veg2, protein = "meat")
-  p "A plate of #{protein} with #{veg1} abd #{veg2}."
+  print "A plate of #{protein} with #{veg1} abd #{veg2}."
 end
